62371ebe1f26e4aeb62652ead89ed04979592198
Don Davis <dondavis.davis55@gmail.com>
2025-08-21 13:32:54 +0100

    'updated'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { SeedComponent } from './seed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:Home
+    },
+    { 
+    path: 'browse-recipes', 
+    loadComponent: () => import('./browse-recipes/browse-recipes').then(m => m.BrowseRecipes)
+    },
+    { 
+    path: 'recipe/:id', 
+    loadComponent: () => import('./recipe-detail/recipe-detail').then(m => m.RecipeDetail)
+    },
+    { path: 'seed', component: SeedComponent },
+    { 
+    path: '**', redirectTo: '' 
+    }
+];
